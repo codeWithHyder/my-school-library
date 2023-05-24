a816dda1175f8b6050e258f26f55f88f5976caac
Hyder <hyderali400@gmail.com>
2023-05-25 00:24:46 +0530

    add associations

--- person.rb
+++ person.rb
@@ -1,29 +1,37 @@
 require_relative 'nameable'
 require_relative 'capitalizedecorator'
 require_relative 'trimmerdecorator'
+require_relative 'rentable'
 
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
-    @age = age
     @name = name
+    @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
     of_age? || @parent_permission == true
   end
 
-  def of_age?
-    @age >= 18
-  end
-
   def correct_name
     @name
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, self, book)
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
   end
 end
 

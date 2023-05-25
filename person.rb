44c1eda72a156eced60a37868429a9eb8c235c55
Hyder <hyderali400@gmail.com>
2023-05-25 23:54:57 +0530

    fix linters

--- person.rb
+++ person.rb
@@ -4,33 +4,34 @@ require_relative 'trimmerdecorator'
 require_relative 'rentable'
 
 class Person < Nameable
-    attr_accessor :name, :age, :rentals, :parent_permission, :classroom
-    attr_reader :id
-    def initialize(age, name, classroom, parent_permission: true)
-        super()
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @classroom =classroom
-        @parent_permission = parent_permission
-        @rentals = []
-    end
+  attr_accessor :name, :age, :rentals, :parent_permission, :classroom
+  attr_reader :id
 
-    def can_use_services?
-        of_age? || @parent_permission == true
-    end
+  def initialize(age, name, classroom, parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @classroom = classroom
+    @parent_permission = parent_permission
+    @rentals = []
+  end
 
-    def correct_name
-        @name
-    end
+  def can_use_services?
+    of_age? || @parent_permission == true
+  end
 
-    def add_rental(book, date)
-        Rental.new(date, self, book)
-    end
+  def correct_name
+    @name
+  end
 
-    private
+  def add_rental(book, date)
+    Rental.new(date, self, book)
+  end
 
-    def of_age?
-        @age >= 18
-    end
+  private
+
+  def of_age?
+    @age >= 18
+  end
 end
